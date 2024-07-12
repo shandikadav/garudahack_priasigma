2f258006b18705b60ba0d6a6e05d2f2f8abcd8b0
difadlyaulhaq <difadlyaulhaq2@gmail.com>
2024-07-12 21:56:26 +0700

    fix : router

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -5,6 +5,7 @@ import 'package:garudahack_priasigmas/ui/pages/meals_page.dart';
 import 'package:garudahack_priasigmas/ui/pages/profile_page.dart';
 import 'package:garudahack_priasigmas/ui/pages/signup_page.dart';
 import 'package:garudahack_priasigmas/ui/pages/snacks_pages.dart';
+import 'package:garudahack_priasigmas/ui/pages/splash_page.dart';
 import 'package:go_router/go_router.dart';
 import '../ui/pages/signin_page.dart';
 // import '../ui/pages/splash_page.dart';
@@ -16,6 +17,26 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: RouteNames.splash,
+      builder: (context, state) => const SplashPage(),
+    ),
+    GoRoute(
+      path: '/signUp',
+      name: RouteNames.signUp,
+      builder: (context, state) =>  SignUpPage(),
+    ),
+    GoRoute(
+      path: '/signIn',
+      name: RouteNames.signIn,
+      builder: (context, state) => const SignInPage(),
+    ),
+    GoRoute(
+      path: '/profile',
+      name: RouteNames.profile,
+      builder: (context, state) => const ProfilePage(),
+    ),
+    GoRoute(
+      path: '/fruit-pages',
       name: RouteNames.fruitspages,
       builder: (context, state) => const FruitsPages(),
       routes: [
@@ -32,27 +53,12 @@ final router = GoRouter(
             );
           },
         ),
-        // GoRoute(
-        //   path: 'meals-Page',
-        //   name: RouteNames.mealsPage,
-        //   builder: (context, state) => const MealsPage(),
-        // ),
+        GoRoute(
+          path: 'meals-Page',
+          name: RouteNames.mealsPage,
+          builder: (context, state) => const MealsPage(),
+        ),
       ],
-    ),
-    GoRoute(
-      path: '/signUp',
-      name: RouteNames.signUp,
-      builder: (context, state) => SignUpPage(),
-    ),
-    GoRoute(
-      path: '/signIn',
-      name: RouteNames.signIn,
-      builder: (context, state) => const SignInPage(),
-    ),
-    GoRoute(
-      path: '/profile',
-      name: RouteNames.profile,
-      builder: (context, state) => const ProfilePage(),
     ),
   ],
 );
